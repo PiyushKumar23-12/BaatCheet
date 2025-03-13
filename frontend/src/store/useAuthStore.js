e1c80b76da2fef035a4e0b296a4956864d23d655
PiyushKumar23-12 <kirandevi010199@gmail.com>
2025-03-13 20:44:26 +0530

    spelling changes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
 
-const BASE_URL= import.meta.env.MODE==="development"?"https://baatcheet-33qm.onrender.com":"/"
+const BASE_URL= import.meta.env.MODE==="development"?"https://baatcheet-33qm.onrender.com/api":"https://baatcheet-33qm.onrender.com/api"
 
 export const useAuthStore=create((set,get)=>({
     authUser:null,
