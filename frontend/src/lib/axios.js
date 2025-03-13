728ed81eb8cc50266db4b9a4f35a4831dfd81b87
PiyushKumar23-12 <kirandevi010199@gmail.com>
2025-03-13 20:49:54 +0530

    spelling changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE==="development"?"https://baatcheet-33qm.onrender.com/api/auth":"/api/auth",
+    baseURL: import.meta.env.MODE==="development"?"https://baatcheet-33qm.onrender.com/api":"https://baatcheet-33qm.onrender.com/api",
     withCredentials: true,
   });
   
